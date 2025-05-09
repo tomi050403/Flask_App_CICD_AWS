e35e7d1f6f02cdc375f134268ad3c056cd281ba9
tomi050403 <toshihiro.mitsutake@gmail.com>
2025-05-09 10:41:19 +0900

    terraform set up flask1

--- 01_terraform/flask-app/environments/dev/main.tf
+++ 01_terraform/flask-app/environments/dev/main.tf
@@ -111,22 +111,22 @@ module "compute" {
 # ---
 # RDS
 # ---
-# module "rds" {
-#   source = "../../modules/rds"
+module "rds" {
+  source = "../../modules/rds"
 
-#   project     = var.project
-#   environment = var.environment
-#   region      = var.region
+  project     = var.project
+  environment = var.environment
+  region      = var.region
 
-#   rds_instance_class  = var.rds_instance_class
-#   rds_username        = var.rds_username
-#   rds_az_none_multiaz = var.rds_az_none_multiaz
-#   rds_db_name         = var.rds_db_name
+  rds_instance_class  = var.rds_instance_class
+  rds_username        = var.rds_username
+  rds_az_none_multiaz = var.rds_az_none_multiaz
+  rds_db_name         = var.rds_db_name
 
-#   private_subnet_1a = module.network.private_subnet_1a
-#   private_subnet_1c = module.network.private_subnet_1c
-#   rds_sg_id         = module.security.rds_sg_id
-# }
+  private_subnet_1a = module.network.private_subnet_1a
+  private_subnet_1c = module.network.private_subnet_1c
+  rds_sg_id         = module.security.rds_sg_id
+}
 
 
 # ---
