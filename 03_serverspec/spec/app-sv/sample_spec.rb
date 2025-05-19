c58c69e0877fff204752ca3e89158af4b23f7916
tomi050403 <toshihiro.mitsutake@gmail.com>
2025-05-19 20:20:02 +0900

    serverspec/rubytest add specdir,spec_helper.rb,Rakefile

---
+++ 03_serverspec/spec/app-sv/sample_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+%w{git gcc zlib-devel bzip2-devel readline-devel sqlite sqlite-devel openssl-devel tk-devel libffi-devel xz-devel}.each do |pkg|
+  describe package(pkg) do
+    it { should be_installed }
+  end
+end
+
+describe command('pyenv --version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /pyenv/ }
+end
+
+describe command('python -V') do
+  its(:stdout) { should match /3\.12\.1/ }
+end
+
+describe command('which python') do
+  its(:stdout) { should match /pyenv/ }
+end
+
+describe command('poetry --version') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match /1\.8\.4/ }
+end
+
+describe command('ls flask-app ') do
+  its(:exit_status) { should eq 0 }
+end
+
+describe file('flask-app/flaskr/.env') do
+	it { should exist }
+  it { should be_file }
+  its(:content) { should match /DB_HOST/ }
+  its(:content) { should match /DB_USER/ }
+  its(:content) { should match /DB_PASSWORD/ }
+  its(:content) { should match /DB_NAME/ }
+  its(:content) { should match /TABLE_NAME/ }
+  its(:content) { should match /ALLOW_FILES/ }
+end
+
+describe command('ps aux | grep "gunicorn.*flaskr:app"') do
+  its(:stdout) { should match /gunicorn.*--daemon/ }
+end
+
+describe port(8000) do
+  it { should be_listening }
+end
+
+describe command('hostname -I') do
+  its(:stdout) { should match /10\.10\.11\./ }
+end
+
---
+++ 03_serverspec/spec/web-sv/sample_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe package('nginx') do
+  it { should be_installed }
+end
+
+describe service('nginx') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/etc/nginx/conf.d/flask-app.conf') do
+  it { should be_file }
+  its(:content) { should match /proxy_pass http:\/\/app.dev.instance.privatelocal:8000;/ }
+end
+
+describe port(80) do
+  it { should be_listening }
+end
+
+describe host('app.dev.instance.privatelocal') do
+  it { should be_resolvable }
+end
+  
+describe command('curl http://app.dev.instance.privatelocal:8000 -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
+end
+
+describe command('hostname -I') do
+  its(:stdout) { should match /10\.10\.12\./ }
+end
+
